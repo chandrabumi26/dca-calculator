ffec27c6fc8046066ccedca344f2b798a98ff4cd
v-dorojatun.c <v-dorojatun.c@bukitmakmur.com>
2024-03-10 11:12:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main">
+    <Navbar></Navbar>
+    <div class="content">Tes</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Navbar from "./components/navbar.vue";
+</script>
+
+<style lang="scss" scoped>
+@import "./assets//main.scss";
+</style>
